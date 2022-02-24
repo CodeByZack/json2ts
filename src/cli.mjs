bb28d57ccc1424fb0f891d043b0a4bcd29ef8665
codebyzack <958059970@qq.com>
2022-02-24 20:49:32 +0800

    添加命令行工具

--- src/cli.mjs
+++ src/cli.mjs
@@ -1,20 +1,29 @@
 #!/usr/bin/env node
 import json2ts from '../dist/index.mjs';
-import 'zx/globals'
+import 'zx/globals';
+import { Command } from 'commander';
+const program = new Command();
 
-const nestedPrimitiveObj = {
-    num: 1,
-    str: 'str',
-    boolean: true,
-    null: null,
-    object: {
-      foo: 1,
-      foo2: false,
-    },
-  };
+program
+  .option('-i, --input <value>', 'input json string')
+  .option('-f, --file <value>', 'input json file name')
+  .option('-o, --output <value>', 'output file name');
 
+program.parse(process.argv);
+const options = program.opts();
 
-// console.log(__dirname);
-// console.log(__filename);
+// 获取输入
+let inputStr = '';
+if(options.input){
+  inputStr = options.input;
+}else if(options.file){
+  inputStr = await fs.readFile(options.file);
+}
 
-fs.outputFile('test.ts',json2ts(JSON.stringify(nestedPrimitiveObj)));
+const resultStr = json2ts(inputStr);
+
+if(options.output){
+  await fs.outputFile(options.output,resultStr);
+}else{
+  console.log(resultStr);
+}
